7c46228abc9a7fcd0ec7b1325e05b58953092aab
Roman <lytkin.roma00@mail.ru>
2020-04-10 22:59:41 +0300

    Доделаны основные функции (разбор на лексемы)
    Осталось сделать нормальный вывод (в файл?)

--- main.cpp
+++ main.cpp
@@ -6,48 +6,43 @@
 
 enum type_of_lex  //кажется, тут далеко не полный список
 {
-    LEX_NULL,     // 
-    LEX_PROGRAM,  // program
-    LEX_ID,       // какой-то индентификатор
-    LEX_INT,      // int (имя типа)
-    LEX_CINT,     // целочисленная константа
-    LEX_CSTRING,  // строковая коснтанта 
-    LEX_CREAL,    // вещественная константа
-    LEX_STRING,   // string (имя типа)
-    LEX_IF,       // if 
-    LEX_ELSE,     // else
-    LEX_PLUS,     // +
-    LEX_MINUS,    // -
-    LEX_WHILE,    // while
-    LEX_LPAREN,   // (
-    LEX_RPAREN,   // )
-    LEX_TIMES,    // *
-    LEX_SLASH,    // / - деление (ну либо часть комментария)
-    LEX_LBRACE,   // {
-    LEX_RBRACE,	  // }
-    LEX_READ,     // read
-    LEX_WRITE,    // write
-    LEX_SEMICOLON,// ; 
-    LEX_NOT,      // not
-    LEX_EQUAL,    // =
-    LEX_NEQUAL,   // !=
-   	LEX_DEQUAL,   // ==
-    LEX_AND,      // and
-    LEX_OR,       // or
-    LEX_GTR,      // > (greater)
-    LEX_GEQ,      // >=
-    LEX_LS,       // < (less)
-    LEX_LEQ,      // <=
-    LEX_FOR, 	  // for
-    LEX_CONT,     // continue
-    LEX_REAL,     // real (имя типа)
-    LEX_POINT,    // . (нужно ли? Ведь она может быть только в составе вещественного числа)
-    LEX_COMMA,    // ,
-    LEX_OPENCOM,  // /*  Вроде бы не нужно, т.к. мы их пропускаем
-    LEX_CLOSECOM, // */  Вроде бы не нужно, т.к. мы их пропускаем
-    LEX_LITERAL,  // запись вида "abc"
-    LEX_FIN       // @
-    // непонятно, стоит ли делать " отдельной лексемой, если она всегда идёт только вместе со строкой 
+    LEX_NULL,     // //0
+    LEX_PROGRAM,  // program //1
+    LEX_ID,       // какой-то индентификатор //2
+    LEX_INT,      // int (имя типа) //3
+    LEX_CINT,     // целочисленная константа //4
+    LEX_CSTRING,  // строковая коснтанта //5
+    LEX_CREAL,    // вещественная константа //6
+    LEX_STRING,   // string (имя типа) //7
+    LEX_IF,       // if //8
+    LEX_ELSE,     // else //9
+    LEX_PLUS,     // + //10
+    LEX_MINUS,    // - //11
+    LEX_WHILE,    // while //12
+    LEX_LPAREN,   // ( //13
+    LEX_RPAREN,   // ) //14
+    LEX_TIMES,    // * //15
+    LEX_SLASH,    // / - деление (ну либо часть комментария) //16
+    LEX_LBRACE,   // { //17
+    LEX_RBRACE,	  // } //18
+    LEX_READ,     // read //19
+    LEX_WRITE,    // write //20
+    LEX_SEMICOLON,// ; //21
+    LEX_NOT,      // not //22
+    LEX_EQUAL,    // = //23
+    LEX_NEQUAL,   // != //24
+   	LEX_DEQUAL,   // == //25
+    LEX_AND,      // and //26
+    LEX_OR,       // or //27
+    LEX_GTR,      // > (greater) //28
+    LEX_GEQ,      // >= //29
+    LEX_LS,       // < (less) //30
+    LEX_LEQ,      // <= //31
+    LEX_FOR, 	  // for //32
+    LEX_CONT,     // continue //33
+    LEX_REAL,     // real (имя типа) //34
+    LEX_COMMA,    // , //35
+    LEX_FIN       // @ //36
 };
 
 // нужно сделать класс Ident - родительским - унаследовать от него строковый класс, класс, описывающий real, и класс, описывающий целые числа
@@ -81,13 +76,26 @@ class Ident
 {
 	char * name;
 	type_of_lex type;
+	bool is_used;
 public:
+	Ident ()
+	{
+		is_used=false;
+	}
+	~Ident ()
+	{
+		if (is_used==true)
+		{
+			delete []name;
+		}
+	}
 	char *get_name ()
 	{
 		return name;
 	}	
 	void put_name (const char * n)
 	{
+		is_used=true;
 		name=new char [strlen(n)+1];
 		strcpy(name, n);
 	}
@@ -302,6 +310,7 @@ Lex Scanner::get_lex ()
 	int lex_pos;
 	int integer;
 	double real;
+	int exp=-1;
 	CS=H;
 	do  //первое чтение осуществлено в конструкторе Scanner
 	{
@@ -309,7 +318,7 @@ Lex Scanner::get_lex ()
 		{
 			case H:
 			{
-				if (c==' '||c=='\n')
+				if (c==' '||c=='\n'||c=='\t')
 				{
 					gc();
 				}
@@ -325,6 +334,49 @@ Lex Scanner::get_lex ()
 					integer=c-'0';
 					gc();
 					CS=NUMB;
+				}
+				else if (c=='<'||c=='>')
+				{
+					clear();
+					add();
+					gc();
+					CS=ALE;
+				}
+				else if (c=='!')
+				{
+					clear();
+					add();
+					gc();
+					CS=EXCLMARK;
+				}
+				else if (c=='=')
+				{
+					clear();
+					add();
+					gc();
+					CS=EQUAL;
+				}
+				else if (c==EOF)
+				{
+					return Lex(LEX_FIN,0);
+				}
+				else if (c=='/')
+				{
+					clear();
+					add();
+					gc();
+					CS=SLASH;
+				}
+				else if (c=='"')
+				{
+					clear();
+					add();
+					gc();
+					CS=STR;
+				}
+				else
+				{
+					CS=DELIM;
 				}
 				break;
 			}
@@ -371,7 +423,6 @@ Lex Scanner::get_lex ()
 			}
 			case REALNUM:
 			{
-				int exp=-1;
 				if (isdigit(c))
 				{
 					real=real+(c-'0')*pow(10,exp--);
@@ -383,6 +434,123 @@ Lex Scanner::get_lex ()
 				}
 				break;
 			}	
+			case ALE:
+			{
+				if (c=='=')
+				{
+					add();
+					gc();
+					lex_pos=look(buf,TD);
+					return Lex(dlms[lex_pos],lex_pos);
+				}
+				else
+				{
+					lex_pos=look(buf,TD);
+					return Lex(dlms[lex_pos],lex_pos);
+				}
+				break;
+			}
+			case EXCLMARK:
+			{
+				if (c=='=')
+				{
+					add();
+					gc();
+					lex_pos=look(buf,TD);
+					return Lex(dlms[lex_pos],lex_pos);
+				}
+				else
+				{
+					throw ("EXCLMARK:ERR");
+				}
+				break;
+			}
+			case EQUAL:
+			{
+				if (c=='=')
+				{
+					add();
+					gc();
+					lex_pos=look(buf,TD);
+					return Lex(dlms[lex_pos],lex_pos);
+				}
+				else
+				{
+					lex_pos=look(buf,TD);
+					return Lex(dlms[lex_pos],lex_pos);
+				}
+				break;
+			}
+			case SLASH:
+			{
+				if (c=='*')
+				{
+					gc();
+					CS=COMMENT;
+				}
+				else
+				{
+					lex_pos=look(buf,TD);
+					return Lex(dlms[lex_pos],lex_pos);
+				}
+				break;
+			}
+			case COMMENT:
+			{
+				if (c==EOF)
+				{
+					throw ("COMMENT:ERR");
+				}
+				else if (c=='*')
+				{
+					gc();
+					if (c=='/')
+					{
+						CS=H;
+						gc();
+					}
+				}
+				else
+				{
+					gc();
+				}
+				break;
+			}
+			case STR:
+			{
+				if (c=='"')
+				{
+					add();
+					gc();
+					return Lex(LEX_CSTRING,0);
+				}
+				else if (c==EOF)
+				{
+					throw ("STR:ERR");
+				}
+				else 
+				{
+					add();
+					gc();
+				}
+				break;
+			}
+			case DELIM:
+			{
+				if (c=='{'||c=='}'||c=='+'||c=='-'||c==','||c==';'||c=='('||c==')'||c=='*')
+				{
+					clear();
+					add();
+					gc();
+					lex_pos=look(buf,TD);
+					return Lex(dlms[lex_pos],lex_pos);
+				}
+				else
+				{
+					throw ("DELIM:ERR");
+				}
+				break;
+			}
 		}
 	} 
 	while (true);
@@ -390,8 +558,16 @@ Lex Scanner::get_lex ()
 
 int main ()
 {
-	char c=EOF;
 	Scanner scan("./prog");
-	std::cout << scan.get_lex() << std::endl;
+	Lex curr_lex;
+	while (true)
+	{
+		curr_lex=scan.get_lex();
+		std::cout << curr_lex << std::endl;
+		if (curr_lex.get_type()==LEX_FIN)
+		{
+			break;
+		}
+	}
 	return 0;
 }
